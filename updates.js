7b7fdf8282a4b175cdfe4af7a63d939a95f72720
David Revay <bumplepump@hotmail.com>
2016-11-04 12:15:26 +1100

    Fix syntax error

--- updates.js
+++ updates.js
@@ -1,8 +1,9 @@
 {
+  {
   "win32-x64-prod": {
     "update": "https://github.com/MrBlenny/STEMN-Desktop/releases/download/v0.0.3-win32",
     "version": "0.0.3",
     "platform": "win32",
     "readme": "Version 3"
-  },
+  }
 }
