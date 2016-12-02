df96220b1acb5c5b15401e59a116b8cb914bd37a
DaveRevay2 <daverevay@gmail.com>
2016-12-02 11:39:40 +1100

    Script fix to create release.json

--- scripts/publish/index.js
+++ scripts/publish/index.js
@@ -6,14 +6,20 @@ const pkg = require('../../package.json')
 const assetsPath = './release/mac';
 
 const publishOsxReleaseFile = (tag) => {
-  console.log('------------------------------------');
-  console.log('Writing release.json to release.json');
+  console.log('Writing release.json to release/mac/release.json...');
   const data = {
     url: `https://github.com/Stemn/Stemn-Desktop/releases/download/v${tag}/Stemn-${tag}-mac.zip`,
     name: '',
     notes: '',
     pub_date: new Date().toISOString()
   };
+  
+  // Create the release/mac dir if it doesn't exist
+  if (!fs.existsSync(assetsPath)){
+      fs.mkdirSync(assetsPath);
+  }
+  
+  // Write release.json to disk
   const releaseFilePath = path.join(assetsPath, 'release.json');
   fs.writeFileSync(releaseFilePath, JSON.stringify(data, null, '  '));
 }
