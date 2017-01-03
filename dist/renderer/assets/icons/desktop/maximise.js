94de69762306c5e312d8d3d7e5fadefca2179883
DaveRevay2 <daverevay@gmail.com>
2017-01-03 19:23:11 +1100

    'release'

---
+++ dist/renderer/assets/icons/desktop/maximise.js
@@ -0,0 +1,13 @@
+
+let React = require('react');
+let IconBase = require('react-icon-base');
+
+export default class MdClose extends React.Component {
+    render() {
+        return (
+            <IconBase viewBox="0 0 40 40" {...this.props}>
+                <path d="M27.215,12.785v14.43h-14.43v-14.43H27.215 M30.465,9.535H9.535v20.93h20.93V9.535L30.465,9.535z"/>
+            </IconBase>
+        );
+    }
+}
---
+++ dist/renderer/assets/other/autodesk/readme.md
@@ -0,0 +1,6 @@
+This will download autodesk viewer files at a specific version.
+
+### Instructions:
+* Set the version in `getViewer.js`
+* `node getViwer.js`
+* The viewer will be downloaded and saved in `./viewer`
