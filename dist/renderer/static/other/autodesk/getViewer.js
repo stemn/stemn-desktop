470378d003c6a06946015172d1fb9408586048a3
DaveRevay2 <daverevay@gmail.com>
2017-05-24 17:14:52 +1000

    release

---
+++ dist/renderer/static/other/autodesk/getViewer.js
@@ -0,0 +1,41 @@
+const viewerFileList = require('./fileList.js');
+const http = require('axios');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+
+// Config - Change this as required
+const version = 'v2.10'
+
+const downloadAndSave = ({url, dest, onProgress}) => {
+  return new Promise((resolve, reject) => {
+    http({
+      url          : url,
+      responseType : 'stream'
+    }).then(response => {
+      // Make directory
+      const path       = dest.substring(0, dest.lastIndexOf('/'));
+      mkdirp(path, () => {
+        const stream     = response.data;
+        const file       = fs.createWriteStream(dest);
+        stream.on('error', response => {
+          fs.unlink(dest);
+          console.log(`Error downloading: ${url}`);
+          reject('error')
+        });
+        stream.on('end', response => {
+          console.log(`Downloaded and saved: ${url}`);
+          resolve('success')
+        });
+        stream.pipe(file);
+      })
+    })
+  })
+};
+
+Promise.all(viewerFileList.map(item => downloadAndSave({
+  url  : `https://developer.api.autodesk.com/viewingservice/v1/viewers/${item}?v=${version}`,
+  dest : './viewer/'+item
+}))).then(response => {
+  console.log('Download complete');
+})
+
---
+++ dist/renderer/static/other/autodesk/readme.md
@@ -0,0 +1,8 @@
+This will download autodesk viewer files at a specific version.
+
+### Instructions:
+* Set the version in `getViewer.js`
+* `node getViewer.js`
+* The viewer will be downloaded and saved in `./viewer`
+
+- Created by David Revay
